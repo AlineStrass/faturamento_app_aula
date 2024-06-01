64cfc0bf392810bd9404faf9556cc78f8da08fce
Aline Strassburger <flores.aline.89@gmail.com>
2024-05-31 22:02:48 -0300

    criação da função de escuta do botão

--- app/src/main/java/com/example/faturamento_app_aula/MainActivity.java
+++ app/src/main/java/com/example/faturamento_app_aula/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.faturamento_app_aula;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NumberPicker numberPicker;  //cria o objeto NumberPicker
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        numberPicker = findViewById(R.id.idNumberPicker); //associar o numberPicker ao elemento de layout
+        numberPicker.setMinValue(2015); // valor minimo que vai apraecer
+        numberPicker.setMaxValue(2024); // valor maximo que vai aparecer
+
+
+        //criando o listener
+        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                Toast.makeText(MainActivity.this, "Teste", Toast.LENGTH_SHORT).show(); // toast serve para apresentar uma mensagem, foi colocada por enquanto, ate fazer a parte logica mesmos
+            }
+        });
+
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+      /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        }); */
     }
 }
